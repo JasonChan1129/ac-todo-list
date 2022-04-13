63a0a077762bb48a1a60151d5668236df1f4eeb7
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-13 17:02:26 +0800

    feat: add model todo isDone params

--- models/todo.js
+++ models/todo.js
@@ -5,6 +5,10 @@ const todoSchema = new Schema({
 		type: String,
 		require: true,
 	},
+	isDone: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 module.exports = mongoose.model('Todo', todoSchema);
