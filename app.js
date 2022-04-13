3ad9f31cf43b9500f4b5bfdcc42f55d07244a00c
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-13 22:19:04 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+
 const routes = require('./routes');
-require('dotenv').config();
+// connect mongoDB
+require('./config/mongoose');
 
 const port = 3000;
-
 const app = express();
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-
-db.on('error', () => {
-	console.log('mongodb error');
-});
-
-db.once('open', () => {
-	console.log('mongodb connected.');
-});
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,17 +1,8 @@
-const mongoose = require('mongoose');
 const Todo = require('../todo');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', () => {
-	console.log('mongodb error');
-});
+// connect mongoDB and return the connection result to db
+const db = require('../../config/mongoose');
 
 db.once('open', () => {
-	console.log('mongodb connected!');
 	for (let i = 0; i < 10; i++) {
 		Todo.create({ name: `name-${i}` });
 	}
