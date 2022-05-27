21b3f6909285b4b553f4e1026bc2318e958be17f
Jason Chan <jasonchan19981017@hotmail.com>
2022-05-27 21:05:39 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const usePassport = require('./config/passport');
 
 const routes = require('./routes');
 // connect mongoDB
@@ -24,6 +25,7 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(routes);
+usePassport(app);
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on http://localhost:${PORT}`);
