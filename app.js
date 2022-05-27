5774f27baa73300c0ac39380312a7be31847e6a1
Jason Chan <jasonchan19981017@hotmail.com>
2022-05-27 17:21:09 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
+const session = require('express-session');
 
 const routes = require('./routes');
 // connect mongoDB
@@ -13,6 +14,13 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 // middlewares
+app.use(
+	session({
+		secret: 'ThisIsMySecret',
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(routes);
