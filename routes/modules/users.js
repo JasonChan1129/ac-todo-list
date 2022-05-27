b697b14888ba34404465d3d8b281cf8baee55b7d
Jason Chan <jasonchan19981017@hotmail.com>
2022-05-27 16:33:49 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// import models
+const User = require('../../models/user');
+
 router.get('/login', (req, res) => {
 	res.render('login');
 });
@@ -9,6 +12,29 @@ router.post('/login', (req, res) => {});
 
 router.get('/register', (req, res) => {
 	res.render('register');
+});
+
+router.post('/register', (req, res) => {
+	const { username, email, password, confirmPassword } = req.body;
+	// only create user data if user does not exists yet.
+	User.findOne({ email })
+		.then(user => {
+			if (user) {
+				console.log('user already existed');
+				return res.render('register', { username, email, password, confirmPassword });
+			} else {
+				// create user data
+				User.create({ username, email, password })
+					.then(() => {
+						console.log('user created');
+						return res.redirect('/');
+					})
+					.catch(err => {
+						console.log(err);
+					});
+			}
+		})
+		.catch(err => console.log(err));
 });
 
 module.exports = router;
