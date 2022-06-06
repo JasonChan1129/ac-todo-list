2ae333a4ed050daeeda56b0adf397124e432f256
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-06 17:43:40 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -23,23 +23,34 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
 	const { username, email, password, confirmPassword } = req.body;
+
+	const errors = [];
+	if (!username || !email || !password || !confirmPassword) {
+		errors.push({ message: '所有欄位都是必填。' });
+	}
+	if (password !== confirmPassword) {
+		errors.push({ message: '密碼與確認密碼不相符!' });
+	}
+	if (errors.length) {
+		return res.render('register', { username, email, password, confirmPassword, errors });
+	}
+
 	// only create user data if user does not exists yet.
 	User.findOne({ email })
 		.then(user => {
 			if (user) {
-				console.log('user already existed');
-				return res.render('register', { username, email, password, confirmPassword });
-			} else {
-				// create user data
-				User.create({ username, email, password })
-					.then(() => {
-						console.log('user created');
-						return res.redirect('/');
-					})
-					.catch(err => {
-						console.log(err);
-					});
+				errors.push({ message: '這個 Email 已經註冊過了。' });
+				return res.render('register', { username, email, password, confirmPassword, errors });
 			}
+			// create user data
+			User.create({ username, email, password })
+				.then(() => {
+					console.log('user created');
+					return res.redirect('/');
+				})
+				.catch(err => {
+					console.log(err);
+				});
 		})
 		.catch(err => console.log(err));
 });
@@ -47,6 +58,7 @@ router.post('/register', (req, res) => {
 router.get('/logout', (req, res) => {
 	req.logout(err => {
 		console.log(err);
+		req.flash('success_msg', '你已經成功登出。');
 		res.redirect('/users/login');
 	});
 });
